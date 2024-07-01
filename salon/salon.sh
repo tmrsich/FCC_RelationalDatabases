eb0fb30c64586b5d13074976d4bc8510fe366c04
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2024-07-01 01:33:50 -0400

    Update salon.sh

--- salon/salon.sh
+++ salon/salon.sh
@@ -2,8 +2,17 @@
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 echo -e "\n~~~ Salon ~~~\n"
 
-$PSQL "TRUNCATE appointments, customers"
+# $PSQL "TRUNCATE appointments, customers"
 AVAILABLE_SERVICES=$($PSQL "SELECT * FROM services ORDER BY service_id;")
+
+  # Repeated above the main menu because if it isn't, the test that checks to see if a list of services are displayed before the first prompt for input fails and so does the one after that
+  echo "How may I help you? Please choose an option from the following list."
+
+	echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR SERVICE
+	do
+		echo "$SERVICE_ID) $SERVICE"
+	done
+		echo "4) Exit"
 
 MAIN_MENU() {
 	if [[ $1 ]]
@@ -31,6 +40,11 @@ MAIN_MENU() {
 	esac
 }
 
+: ' Method that confirms the appointment of the customer. It first asks the phone number of the user to check to see if it exists in the customers table
+      - If it doesnt, it asks for the customers name, reads it into a variable, and inserts both the customers phone number and their name into the customers table with an auto-incrementing customer_id
+      - If it does, meaning that they are a previous customer and they are in the database, 
+
+'
 CONFIRM_APPOINTMENT() {	
 	echo -e "\nWhat is your phone number?"
 	read CUSTOMER_PHONE
@@ -43,7 +57,7 @@ CONFIRM_APPOINTMENT() {
 		echo -e "\nWhat is your name?"
 		read CUSTOMER_NAME
 
-		sleep 2
+		# sleep 2
 
 		$PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE');"
 
@@ -60,16 +74,19 @@ CONFIRM_APPOINTMENT() {
 	# Inserts the appointment into the appointments table
 	$PSQL "INSERT INTO appointments(service_id, customer_id, time) VALUES('$SERVICE_ID_SELECTED', '$CUSTOMER_ID', '$SERVICE_TIME')"
 
-	# Output message - COMPLETE 
+	# Output message - TESTS PASS
 
 	# Get the name of the service from the service_id - Use PSQL command
-	SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED;");
+	SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED;")
+
+  # Gets the name of the customer from the database if a name wasn't previously entered due to them already being in the system
+  CUSTOMER_NAME2=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE';")
 	
 	# Final message
-	echo -e "\nI have you put down for a$SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME"
+	echo -e "\nI have put you down for a$SERVICE_NAME at $SERVICE_TIME,$CUSTOMER_NAME2."
 
 	# Send to main menu again
-	MAIN_MENU
+  # MAIN_MENU
 }
 
 EXIT() {
